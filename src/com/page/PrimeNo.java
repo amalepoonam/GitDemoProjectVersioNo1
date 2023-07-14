8f792726c1e0add6838865b1ca6839ef70ea32fc
Poonam <Poonam@Poonam>
2023-07-14 13:12:35 +0530

    Commit

---
+++ src/com/page/PrimeNo.java
@@ -0,0 +1,25 @@
+package com.page;
+
+public class PrimeNo {
+	
+	public static void main(String[] args) {
+		int a=17;
+		int cnt=0;
+		
+			if(a%2==0) {
+				cnt++;
+				
+			}
+			if(cnt==2)
+			
+				System.out.println(a+"is prime");
+				else 
+					System.out.println(a+"not prime");
+			}
+			}
+
+		
+	
+	
+
+
